d6936b23521f873bcf1cfacbeab374b66d05c4e9
Lucas Quinn <lucas.j.quinn@outlook.com>
2023-12-12 17:03:42 +1300

    Add @ionic/core dependency and update component rendering

--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,32 +1,37 @@
-import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
+import { Component, State, h } from '@stencil/core';
+
 
 @Component({
   tag: 'my-component',
-  styleUrl: 'my-component.css',
-  shadow: true,
+  // shadow: true,
 })
 export class MyComponent {
-  /**
-   * The first name
-   */
-  @Prop() first: string;
 
-  /**
-   * The middle name
-   */
-  @Prop() middle: string;
+  @State()
+  isOpen = false;
 
-  /**
-   * The last name
-   */
-  @Prop() last: string;
 
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
+  handler = () => {
+    // we can set internal to trigger this indifinitely so you don't need to click
+
+
+    // setInterval(() => {
+    //   this.isOpen = !this.isOpen;
+    // }, 100);
+
+
+    this.isOpen = !this.isOpen;
   }
 
+
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return (
+      <div>
+        <button onClick={this.handler}>Trigger Leak</button>
+        {/* ion-button can be other element like ion-searchbar. and can be other stencil component too. */}
+        <br />
+        {this.isOpen && <ion-button>here</ion-button>}
+      </div>
+    );
   }
 }
